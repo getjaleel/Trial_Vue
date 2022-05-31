5cb6707565885c40da146cf8217a3dbaf7bfb592
Jaleel Ahmed Mohammad <16719397+getjaleel@users.noreply.github.com>
2022-05-31 10:35:09 +0530

    Create aws-exports.js

---
+++ src/aws-exports.js
@@ -0,0 +1,28 @@
+const awsmobile = {
+    "aws_project_region": "ap-southeast-2",
+    "aws_cognito_identity_pool_id": "",
+    "aws_cognito_region": "ap-southeast-2",
+    "aws_user_pools_id": "ap-southeast-2_wzkkzuUGk",
+    "aws_user_pools_web_client_id": "5q6qb38ubmv57d1at1olv326vu",
+    "oauth": {},
+    "aws_cognito_username_attributes": [],
+    "aws_cognito_social_providers": [],
+    "aws_cognito_signup_attributes": [
+        "EMAIL",
+        "NICKNAME"
+    ],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": [
+        "SMS"
+    ],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
+        "passwordPolicyCharacters": []
+    },
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ]
+};
+
+
+export default awsmobile;

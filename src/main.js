3775de5c2ceb2f480cbcac3cfcce78c7a27fe203
Jaleel Ahmed Mohammad <16719397+getjaleel@users.noreply.github.com>
2022-05-31 13:05:23 +1000

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "@aws-amplify/ui-vue/styles.css";
+
+// window.alert(
+//   "Once filled the Account Creation form,You will be presented with OTP 'On Its way' window.Please close the page and wait until and approval email is recieved in your registered email "
+// );
+createApp(App).mount("#app");

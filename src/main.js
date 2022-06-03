7b4cf477ac67dfd5261e31c29823fc11f47090b5
Jaleel Ahmed Mohammad <16719397+getjaleel@users.noreply.github.com>
2022-06-03 10:06:20 +0530

    Close button

--- src/main.js
+++ src/main.js
@@ -6,8 +6,9 @@ import "@aws-amplify/ui-vue/styles.css";
 //   "Once filled the Account Creation form,You will be presented with OTP 'On Its way' window.Please close the page and wait until and approval email is recieved in your registered email "
 // );
 
-
+<script>
 <button type="button" 
     onclick="window.open('', '_self', ''); window.close();">Close</button>
+</script>
 
 createApp(App).mount("#app");

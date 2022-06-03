fcd307d63239a6ae059ce80ea48c9ae965a02394
Jaleel Ahmed Mohammad <16719397+getjaleel@users.noreply.github.com>
2022-06-03 10:05:22 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,4 +5,9 @@ import "@aws-amplify/ui-vue/styles.css";
 // window.alert(
 //   "Once filled the Account Creation form,You will be presented with OTP 'On Its way' window.Please close the page and wait until and approval email is recieved in your registered email "
 // );
+
+
+<button type="button" 
+    onclick="window.open('', '_self', ''); window.close();">Close</button>
+
 createApp(App).mount("#app");

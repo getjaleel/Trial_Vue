ace615bbd9b04611df9fe2a3c999e1526ef17b9e
Jaleel Ahmed Mohammad <16719397+getjaleel@users.noreply.github.com>
2022-06-06 16:41:01 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,7 @@
       <div class="amplify-flex" style="flex-direction: column;">
         <h3
           class="amplify-heading"
-          style="padding: var(--amplify-space-xl) 0 0 var(--amplify-space-xl); color: red"
+          style="padding: var(--amplify-space-xl) ; color: red"
         >
           NO CODE IS SENT OUT
         </h3>
